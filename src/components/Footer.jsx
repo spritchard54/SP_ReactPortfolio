3ea95381e90ad3b4173c62d915b07a9eb6e9db64
spritchard54 <>
2026-03-27 14:45:02 -0400

    added sticky footer to the page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export function Footer() {
+  return (
+    <>
+      <footer className="mt-auto py-3 bg-light">
+        <div className="container-fluid">
+          <span className="text-muted">Copyright Steve</span>
+        </div>
+      </footer>
+    </>
+  );
+}
