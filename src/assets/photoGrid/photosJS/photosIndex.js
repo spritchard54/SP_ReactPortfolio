649d03e40c7345330e913a931392ee9fe9910897
spritchard54 <>
2026-04-01 14:25:02 -0400

    Layout started for photos page. Folder cleanup performed. Public moved out of SRC and images moved to assets folder.

---
+++ src/assets/photoGrid/photosJS/photosIndex.js
@@ -0,0 +1,31 @@
+const Images = [
+
+  {
+    id:1,
+    image: 'src/assets/photoGrid/photosPage/IMG_4036.jpeg',
+    alt: 'Deer Park, Olympic National Park'
+  },
+  {
+    id:2,
+    image: 'src/assets/photoGrid/photosPage/IMG_4583.jpeg',
+    alt: 'Mount Rainer National Park'
+  },
+  {
+    id:3,
+    image: 'src/assets/photoGrid/photosPage/IMG_3568.jpeg',
+    alt: 'Devils Tower, WY'
+  },
+  {
+    id:4,
+    image: 'src/assets/photoGrid/photosPage/IMG_3635.jpeg',
+    alt: 'Olympic National Park'
+  },
+  {
+    id:5,
+    image: 'src/assets/photoGrid/photosPage/IMG_4843.jpeg',
+    alt: 'Grand Teton natipnal Park'
+  },
+  
+]
+
+export default Images;
--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,7 +1,56 @@
+import Images from "../assets/photoGrid/photosJS/photosIndex";
+
 export function Photos() {
+  const imagesData = Images;
+
+  // Ensure we have a list of 4 arrays each containing up to 7 image URLs
+  const cols = Array.from({ length: 4 }, (_, colIndex) =>
+    imagesData.slice(colIndex * 7, colIndex * 7 + 7),
+  );
+
   return (
+    // <div className="container-fluid">
+    //   <h1 className="my-3">Welcome to my photos page...</h1>
+
+    //   <div className="row">
+    //     {imagesData.map((photo) => (
+    //       // Col component defines the column width for different screen sizes
+    //       // xs={12}: 1 column on extra small screens
+    //       // sm={6}: 2 columns on small screens
+    //       // md={4}: 3 columns on medium screens
+    //       // lg={3}: 4 columns on large screens
+
+    //       <div className="col-md-3 my-1 mx-0 px-1" key={photo.id}>
+    //         <img
+    //           src={photo.image}
+    //           alt={photo.alt}
+    //           className="img-fluid" />
+    //       </div>
+    //     )
+    //   )
+    // }
+    //   </div>
+    // </div>
+
     <>
-      <h1>This is the Photos Page.</h1>
+      <div className="container-fluid">
+        <div className="row">
+          {cols.map((imagesData, id) => (
+            <div className="col-12 col-md-3" key={id}>
+              {imagesData.map((photo) => (
+                <img
+                  key={photo.id}
+                  src={photo.image}
+                  // alt={`col-${id}-img-${id}`}
+                  className="img-fluid mb-2 w-100"
+                  // optional: style or wrapper to control height/cropping
+                  style={{ objectFit: "cover" }}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
