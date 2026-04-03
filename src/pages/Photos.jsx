02bd289150acc4c9688da8a303e6076d15d45519
spritchard54 <>
2026-04-03 14:12:44 -0400

    Added in srcset and sizes attributes.

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -10,47 +10,27 @@ export function Photos() {
   );
 
   return (
-    // <div className="container-fluid">
-    //   <h1 className="my-3">Welcome to my photos page...</h1>
-
-    //   <div className="row">
-    //     {imagesData.map((photo) => (
-    //       // Col component defines the column width for different screen sizes
-    //       // xs={12}: 1 column on extra small screens
-    //       // sm={6}: 2 columns on small screens
-    //       // md={4}: 3 columns on medium screens
-    //       // lg={3}: 4 columns on large screens
-
-    //       <div className="col-md-3 my-1 mx-0 px-1" key={photo.id}>
-    //         <img
-    //           src={photo.image}
-    //           alt={photo.alt}
-    //           className="img-fluid" />
-    //       </div>
-    //     )
-    //   )
-    // }
-    //   </div>
-    // </div>
-
     <>
       <div className="container-fluid">
         <div className="row">
           <h2 className="mt-3">Summer Roadtrip &apos;21</h2>
           <p>
-            Not long long ago I built this page using the code I found{" "}
-            <Link to="https://www.w3schools.com/howto/howto_css_image_grid_responsive.asp">
+            Not long long ago I built this page using the code I found <Link to="https://www.w3schools.com/howto/howto_css_image_grid_responsive.asp" target='_blank'>
               here
             </Link>{" "}
-            on the <Link to="https://www.w3schools.com/">W3 School's</Link> website. This worked great on a static website, and was easy enough to
-            implement. Now that the site has been rebuilt using React, I had to take a
-            slightly different approach. Essentially, I wrote a function that created a 4 column layout, and
-            then used the .map&#40;&#41; method on that function to load the
-            photos into those columns. It took a bit of time to figure it out, but ultimately
+            on the <Link to="https://www.w3schools.com/" target='_blank'>W3 School's</Link> website. This worked great on a static website, and was easy enough
+            to implement. Now that the site has been rebuilt using React, I had
+            to take a slightly different approach. Essentially, I wrote a
+            function that created a 4 column layout, and then used the
+            .map&#40;&#41; method on that function to load the photos into those
+            columns. It took a bit of time to figure it out, but ultimately
             resulted in far less code and code that was much easier to read.
           </p>
           <p>
-            All of the images below were taken on a road trip I went on in the summer of 2021. As I was pulling this together, I couldn't help but think about how great that trip was, and how I hope to have the opportunity to go on a simlar trip some day in the future.
+            All of the images below were taken on a road trip I went on in the
+            summer of 2021. As I was pulling this together, I couldn't help but
+            think about how great that trip was, and how I hope to have the
+            opportunity to go on a simlar trip some day in the future.
           </p>
           {cols.map((imagesData, id) => (
             <div className="col-sm-12 col-md-6 col-lg-3 gx-2" key={id}>
@@ -58,10 +38,12 @@ export function Photos() {
                 <img
                   key={photo.id}
                   src={photo.image}
+                  srcSet={photo.srcset}
+                  sizes={photo.sizes}
                   alt={photo.alt}
                   className="img-fluid mb-2 w-100"
                   // optional: style or wrapper to control height/cropping
-                  style={{ objectFit: "cover" }}
+                  // style={{ objectFit: "cover" }}
                 />
               ))}
             </div>
