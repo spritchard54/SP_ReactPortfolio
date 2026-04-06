dc2b79d69ab963d4aabaa22df471cc0d4b405fc4
spritchard54 <>
2026-04-06 17:49:40 -0400

    Additioanl leaflet js code added.

---
+++ src/assets/js/mapLocations.js
@@ -0,0 +1,22 @@
+const mapLocations = [
+  {
+    id: "1",
+    geocode: [41.03875493733112, -75.08159465465165],
+    popUp: "Shawnee Ski Resort",
+    iconType: 'skiing' 
+  },
+  {
+    id: "2",
+    geocode: [41.05162999763743, -75.35604835434484],
+    popUp: "Camelback Ski Resort",
+    iconType: "skiing",
+  },
+  {
+    id: "3",
+    geocode: [41.35102796478296, -75.66220855992954],
+    popUp: "Montage Mountain Ski Resort",
+    iconType: "",
+  },
+];
+
+export default mapLocations;
