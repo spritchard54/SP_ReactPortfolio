f20a8e79ac1de61ab48a1d7bba302bb612d1fcb9
spritchard54 <swpritchard54@gmail.com>
2026-04-11 10:28:57 -0400

    Updated text content on trips page and included links to Leaflet and React Leaflet

--- src/pages/Trips.jsx
+++ src/pages/Trips.jsx
@@ -50,17 +50,24 @@ export function Trips() {
           <h1>Where I've Been</h1>
           <p>
             A friend of mine has a "places you've been map" hanging in his
-            house. I liked the idea of having something like that, but wanted
+            house. I liked the idea of having something like that, but I wanted
             something a bit more customizable so I built this page. Like most of
-            the site, it's still a work in progress. The map and markers are
-            enabled by Leaflet which by their own account "is the leading
-            open-source JavaScript library for mobile-friendly interactive
-            maps." At any rate; I like it. You can hover over the icon in the
-            top right corner of the map then toggle between various layers, and
-            which markers you want to display on the map. I'm just getting
-            started with this and don't have content for every location yet, but
-            over time I hope to get there. In the meantime, see if you know
-            where Yosemite National Park is and click on 'Details...'.
+            the site, it's still a work in progress.
+          </p>
+          <p>
+            The map and markers are enabled by <Link to='https://leafletjs.com/' target="_blank" rel="noopener noreferrer"> Leaflet </Link> which by their own
+            account "is the leading open-source JavaScript library for
+            mobile-friendly interactive maps." In order to have this work with React I also needed to install <Link to='https://react-leaflet.js.org/' target="_blank" rel="noopener noreferrer">React Leaflet</Link>.
+          </p>
+
+          <p>
+            At any rate; I like it. You can
+            hover over the icon in the top right corner of the map then toggle
+            between various layers, and which markers you want to display on the
+            map. I'm just getting started with this and don't have content for every
+            location yet, but over time I hope to get there. In the meantime,
+            see if you know where Yosemite National Park is and click on
+            'Open Trip...'.
           </p>
         </div>
         <div className="row">
