fe91a10a246ec8a00c07e638a32467dff2f53921
spritchard54 <swpritchard54@gmail.com>
2026-05-12 14:47:46 -0400

    config changes for render hosting

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <HelmetProvider>
+    <HelmetProvider>
+      <BrowserRouter>
         <App />
-      </HelmetProvider>
-    </HashRouter>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 );
