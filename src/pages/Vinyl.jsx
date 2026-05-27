ca7c22b26aec75cccd28c84ed5d1dde82aab585d
spritchard54 <swpritchard54@gmail.com>
2026-05-27 13:51:10 -0400

    Updated vinylrecord.js to .json, added more records, updated formatting on page.

--- src/pages/Vinyl.jsx
+++ src/pages/Vinyl.jsx
@@ -1,56 +1,61 @@
-import { vinylRecords } from "../assets/js/vinylRecords";
+import vinylRecords from "../assets/js/vinylRecords.json";
 
 function Vinyl() {
   return (
     <div className="container my-4">
       <h1>Vinyl Collection</h1>
       <p>A small selection of albums from my personal record collection.</p>
-      <div className="row">
-        {vinylRecords.map((record) => (
-          <div className="col-12 col-md-6 col-lg-4 mb-4" key={record._id.$oid}>
-            <div className="card h-100">
-              <div className="card-body">
-                {record.artwork ? (
-                  <img
-                    src={record.artwork}
-                    className="card-img-top mb-3"
-                    alt="..."
-                    loading="lazy"
-                  />
-                ) : (
-                  <span>Image coming soon...</span>
-                )}
-                <h3 className="card-title">{record.title}</h3>
-                <h5 className="card-subtitle mb-2 text-muted">
-                  {record.artist}
-                </h5>
-                <p className="card-text">
-                  <strong>Album Name:</strong> {record.albumName}
-                </p>
-                <p className="card-text">
-                  <strong>Genre:</strong>{" "}
-                  {Array.isArray(record.genre) ? (
-                    record.genre.map((genre) => (
-                      <span
-                        key={genre}
-                        className="badge text-bg-secondary me-2"
-                      >
-                        {genre}
-                      </span>
-                    ))
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-5 g-2">
+        {[...vinylRecords]
+          .sort((a, b) =>
+            a.artist
+              .replace(/The /i, "")
+              .localeCompare(b.artist.replace(/^The /i, "")),
+          )
+          .map((record) => (
+            <div className="col" key={record._id.$oid}>
+              <div className="card h-100">
+                <div className="card-body">
+                  {record.artwork ? (
+                    <img
+                      src={record.artwork}
+                      className="card-img-top mb-3"
+                      alt="..."
+                      loading="lazy"
+                    />
                   ) : (
-                    <span className="badge text-bg-secondary me-2">
-                      {record.genre}
-                    </span>
+                    <div className="mb-3">Image coming soon...</div>
                   )}
-                </p>
-                <p className="card-text">
-                  <strong>Label:</strong> {record.label}
-                </p>
+                  <h5 className="card-subtitle mb-2 text-muted">
+                    {record.artist}
+                  </h5>
+                  <p className="card-text">
+                    <strong>Album Name:</strong> {record.albumName}
+                  </p>
+                  <p className="card-text">
+                    <strong>Genre:</strong>{" "}
+                    {Array.isArray(record.genre) ? (
+                      record.genre.map((genre) => (
+                        <span
+                          key={genre}
+                          className="badge text-bg-secondary me-2"
+                        >
+                          {genre}
+                        </span>
+                      ))
+                    ) : (
+                      <span className="badge text-bg-secondary me-2">
+                        {record.genre}
+                      </span>
+                    )}
+                  </p>
+                  <p className="card-text">
+                    <strong>Label:</strong> {record.label}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
